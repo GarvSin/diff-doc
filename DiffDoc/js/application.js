e35a610b4ab1bcedfdc7611f41335894ce8b6983
Gaurav Singh <singh.gaurav.cs@gmail.com>
2019-03-27 23:23:16 +0000

    Update application.js

--- DiffDoc/js/application.js
+++ DiffDoc/js/application.js
@@ -6,5 +6,6 @@ var showdiff = function (){
                             var text1 = $('#filePanel1 .panel-body pre').text();
                             var text2 = $('#filePanel2 .panel-body pre').text();
                             var diff = diffString(text1,text2);
+                            var diff = diffString(text1,text2);
                             $('#compareResult').html(diff);
                           }
